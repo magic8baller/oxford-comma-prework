96e8d6c3df04b03cc65953d65e19c585bf901d6f
Leslie Turis <lturis@gmail.com>
2018-09-02 03:43:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    array.join(" and ")
+    "#{array[0]} and #{array[1]}"
   elsif array.length >= 3
     sliced_string = "#{array[0..-2].join(", ")}, and #{array[-1]}"
     sliced_string
