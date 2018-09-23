03f8910a7fe3e22d10d1ba089baafdf20b58cfeb
magic8baller <39971956+magic8baller@users.noreply.github.com>
2018-09-22 22:32:21 -0400

    Update oxford_comma.rb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  elsif array.length == 2
-    "#{array[0]} and #{array[1]}"
-  elsif array.length > 2
-    "#{array[0..-2].join(", ")}, and #{array[-1]}"
+length = array.length
+  if length == 1
+   array.join 
+  elsif length == 2
+    array.join(' and ')
+  elsif length > 2
+    "#{array[0..-2].join(', ')}, and #{array[-1]}"
   end
 end
